fe4b570d61590bef7e6c61feed3090b13f4c5161
jirijancik <jirijancik2@gmail.com>
2019-06-04 21:44:40 +0200

    project created for react --typescript

---
+++ src/components/MyClass.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyClass extends React.Component {
+  render(){
+      return(
+          <div>
+              HELLO WORLD!
+          </div>
+      )
+  }
+}
+
+export default MyClass;
