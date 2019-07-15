4fb2b7ac0aa005abcb0739deb146e985e5d53e34
jirijancik <jirijancik2@gmail.com>
2019-07-16 00:02:03 +0200

    redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import LoginTable from './components/LoginTable';
+import {useSelector, useDispatch} from 'react-redux';
+import {signIn} from './redux/actions'
 
 const App: React.FC = () => {
+
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
 
 //TODO: přepsat na class component - asi
 //  -component should update -> render Register Form and remove Login Table and vice versa
 
   return (
     <Fragment>
+          <h1>Counter {counter}</h1>
+          <button onClick={() => dispatch(signIn())}>+</button>
     <LoginTable></LoginTable>
     </Fragment>
   );
